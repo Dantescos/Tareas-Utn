723609f72e5e032f42e68601164f7611887760ed
Rem <Antoniougm789@gmail.com>
2023-11-23 02:36:05 -0300

    se avanzo con los componentes de items luego de terminar los otros

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import TareaFormulario from "./Componentes/TaskForm";
+
 
 function App() {
   return (
     <div className="App">
-      
+
+
+
+
+    <div className='tareas-listado'>
+      <h1 className='titulo'> Mis tareas </h1>
+        <TareaFormulario/>
+    </div>  
     </div>
+    
   );
 }
 
